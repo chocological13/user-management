4665459a8c266c745e9ad6b169df2eff4b93352f
nina <annisamustofa@outlook.com>
2024-05-06 19:45:26 +0700

    feat: updated color scheme, todo: handle responsiveness

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const NavBar: React.FC = () => {
   return (
-    <div className="navigation relative bg-green-500 grid grid-cols-3 justify-center items-center h-16 px-16 mb-20">
+    <div className="navigation relative bg-[#A87676] grid lg:grid-cols-3 md:grid-cols-2 justify-center items-center lg:h-16 sm:h-12 lg:px-16 mb-20">
       <div>
-        <a href="#" className="text-left pr-4 relative items-center p-2 bg-white w-26 rounded-md font-bold text-2xl">
+        <a href="#" className="text-left pr-4 relative items-center p-2 bg-[#F5EEE6] w-26 rounded-md font-bold text-2xl">
           Network Call Practice
         </a>
       </div>
-      <div className="links flex justify-center items-center h-full gap-2 text-xl">
+      <div className="links flex justify-center items-center h-full gap-2 text-xl md:flex-col">
         <a href="/" className="px-2">
           Users
         </a>
